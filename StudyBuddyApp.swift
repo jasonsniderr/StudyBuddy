6e6022eb256c8ad8166307bd8b9399da49c561ab
jasonsniderr <jason123snider@gmail.com>
2025-11-17 09:30:22 -0700

    Initial commit

---
+++ StudyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBuddyApp.swift
+//  StudyBuddy
+//
+//  Created by Jason Snider on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct StudyBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+          Text("Setup your app dummy")
+        }
+    }
+}
